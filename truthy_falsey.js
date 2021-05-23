9697495412b97af1702f5df5b2c7cba91db35eb8
asifhossain2k20 <59142263+asifhossain2k20@users.noreply.github.com>
2021-05-23 23:31:37 +0600

    done

---
+++ truthy_falsey.js
@@ -0,0 +1,21 @@
+
+const f;
+//falsy
+//f=false;
+//f=0;
+//f=""
+//f=null;
+//f=NaN;
+
+const t;
+//truthy
+//let arr=[];
+//let obj=[];
+
+if(t){
+    console.log("truth");
+}
+else{
+    console.log("Falsy");
+}
+
