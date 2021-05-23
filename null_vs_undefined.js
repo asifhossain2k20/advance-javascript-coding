3cdd31f800f9288b0ca09d815dcaced311d12845
asifhossain2k20 <59142263+asifhossain2k20@users.noreply.github.com>
2021-05-23 23:47:48 +0600

    null vs undefined

---
+++ null_vs_undefined.js
@@ -0,0 +1,18 @@
+//undefined
+
+let value;
+console.log(value);
+
+let arr=[12,13,45];
+console.log(arr[10]);
+
+let obj={name:"Asif"}
+console.group(obj.phoneNo);
+
+function add(a,b){
+    console.log(a,b);
+    return
+}
+
+const result=add(10);
+console.log(result);
